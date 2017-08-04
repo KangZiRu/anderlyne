9203f001b85df8b75010b6333c4f429f9adac27e
Prasna Lukito <prasnakuxing@gmail.com>
2017-08-04 10:01:56 +0700

    some little changes

--- anderlyne.js
+++ anderlyne.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
 Anderlyne CSS Framework
 
@@ -29,30 +31,29 @@ SOFTWARE.
 var contextMenu,
     $context = [];
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("[data-watch='height']").anderlyne("height");
 
-    $(".dropdown").click(function() {
+    $(".dropdown").click(function () {
         var that = $(this),
             dDId = that.attr("data-dropdown-id");
 
         $("*[data-dropdown='" + dDId + "']").slideToggle();
-
     });
 
-    $.each($(".select-group"), function(i) {
+    $.each($(".select-group"), function (i) {
         if ($(this).find("[data-selected]").length == 0) {
             $(this).find("[data-value]:first-of-type").attr("data-selected", true);
         }
-    })
+    });
 
-    $(".select-group *[data-group]").click(function() {
+    $(".select-group *[data-group]").click(function () {
         $(this).parent().find("*").removeAttr("data-selected");
         $(this).attr("data-selected", true);
     });
 
-    $(".slider-item").click(function() {}).mousemove(function() {
-        console.log("dmqwoidw")
+    $(".slider-item").click(function () {}).mousemove(function () {
+        console.log("dmqwoidw");
     });
 
     $("body").append('\
@@ -74,19 +75,18 @@ $(document).ready(function() {
 </div>\
 <div id="anderlyne-title"></div>');
 
-    $("#anderlyne-notif-bar *[data-dismiss]").click(function() {
+    $("#anderlyne-notif-bar *[data-dismiss]").click(function () {
         $("#anderlyne-notif-bar").css("left", "-1000px");
-    })
+    });
     $(document).anderlyne("main");
 });
 
-
 jQuery.fn.extend({
-    anderlyne: function(func, param1) {
+    anderlyne: function anderlyne(func, param1) {
         var a = this;
 
         main = {
-            modal: function(x) {
+            modal: function modal(x) {
                 if (typeof x == 'undefined') {
                     x = "n";
                 }
@@ -96,15 +96,17 @@ jQuery.fn.extend({
                 } else if (x == "open") {
                     a.fadeIn();
                     a.children('.content').slideDown();
+                    $(a).trigger("modalOpened");
                 } else if (x == "close") {
                     a.fadeOut();
                     a.children('.content').slideUp();
+                    $(a).trigger("modalClosed");
                 }
                 return a;
             },
-            height: function() {
+            height: function height() {
                 var el = a.children();
-                var z = el.map(function() {
+                var z = el.map(function () {
                     return $(this).height();
                 }).get();
 
@@ -112,39 +114,35 @@ jQuery.fn.extend({
 
                 return el.height(max);
             },
-            clearInput() {
-                e.filter("input[type='text'], input[type='password'], textarea").val("");
-                e.filter("select").find("option").removeAttr("selected");
-                e.filter("select").find("option:first-child").attr("selected", true);
+            clearInput: function clearInput() {
+                a.find("input[type='text'], input[type='password'], input[type='number'], textarea").val("");
+                a.find("select").find("option").removeAttr("selected");
+                a.find("select").find("option:first-child").attr("selected", true);
             },
-            main: function(x) {
+            main: function main(x) {
                 if (typeof x == "undefined") {
-                    x = "*";
+                    x = document;
                 }
 
-                var data0 = x + " div[data-modal]",
-                    data1 = x + " [data-modal-close]",
-                    data2 = x + " [data-open-modal]";
-
-                $(data0).click(function(event) {
+                $(x).on("click", "div[data-modal]", function (event) {
                     var that = $(this);
                     if (event.target.id == $(this).attr("id")) {
                         that.anderlyne("modal", "close");
                     }
                 });
 
-                $(data1).click(function() {
+                $(x).on("click", "[data-modal-close]", function () {
                     var modal = $(this).parents("[data-modal]");
                     modal.anderlyne("modal", "close");
                     return false;
                 });
 
-                $(data2).click(function() {
+                $(x).on("click", "[data-open-modal]", function () {
                     var modal = $(this).attr("data-open-modal");
                     $("#" + modal).anderlyne("modal", "open");
                 });
 
-                $(x + "[data-title]").mouseenter(function(e) {
+                $(x).on("mouseenter", "[data-title]", function (e) {
                     $("#anderlyne-title").html($(this).attr("data-title"));
                     $("#anderlyne-title").show();
                     if (e.pageX >= window.innerWidth - 150) {
@@ -158,7 +156,7 @@ jQuery.fn.extend({
                     });
                 });
 
-                $(x + '[data-title]').mousemove(function(e) {
+                $(x).on("mousemove", '[data-title]', function (e) {
                     if (e.pageX >= window.innerWidth - $("#anderlyne-title").width() - 40) {
                         var leftPos = e.pageX - $("#anderlyne-title").width();
                     } else {
@@ -170,7 +168,7 @@ jQuery.fn.extend({
                     });
                 });
 
-                $(x + "#anderlyne-title").mousemove(function(e) {
+                $(x).on("mousemove", "#anderlyne-title", function (e) {
                     if (e.pageX >= window.innerWidth - $(this).width()) {
                         var leftPos = e.pageX - $("#anderlyne-title").width() - 10;
                     } else {
@@ -182,18 +180,18 @@ jQuery.fn.extend({
                     });
                 });
 
-                $(x + '[data-title]').mouseleave(function() {
+                $(x).on("mouseleave", '[data-title]', function () {
                     $("#anderlyne-title").hide();
                     $("#anderlyne-title").html("");
                 });
 
-                $(document).keyup(function(event) {
-                    if (event.which == 27 && ($("div[data-modal]:visible").length > 0) && $("input:focus, select:focus").length == 0) {
+                $(document).keyup(function (event) {
+                    if (event.which == 27 && $("div[data-modal]:visible").length > 0 && $("input:focus, select:focus").length == 0) {
                         $("div[data-modal]:visible").anderlyne("modal", "close");
                     }
                 });
 
-                $(x + "[data-open-context]").contextmenu(function(e) {
+                $(x).on("contextmenu", "[data-open-context]", function (e) {
                     contextMenu = $("#" + $(this).attr("data-open-context"))[0];
 
                     $context.currentTarget = e.currentTarget;
@@ -212,24 +210,14 @@ jQuery.fn.extend({
                     }).show();
 
                     return false;
-                })
-
-                $(document).click(function(e) {
-                    $("[data-context-menu]")
-                        .hide()
-                        .offset({
-                            x: 0,
-                            y: 0
-                        });
                 });
 
-                $("[data-dropdown-menu]").click(function(e) {
-                    var att = $(this).attr("data-dropdown-menu")
-                    t = $("#" + att),
-                        that = $(this);
+                $(document).on("click", "[data-dropdown-menu]", function (e) {
+                    var att = $(this).attr("data-dropdown-menu");
+                    t = $("#" + att), that = $(this);
                     t.css({
                         top: that.position().top + 4,
-                        left: that.position().left,
+                        left: that.position().left
                     });
 
                     $("[data-dropdown][id!='" + att + "']").hide();
@@ -238,11 +226,15 @@ jQuery.fn.extend({
                     return false;
                 });
 
-                $(window).click(function(e) {
+                $(window).click(function (e) {
                     $("[data-dropdown]").hide();
+                    $("[data-context-menu]").hide().offset({
+                        x: 0,
+                        y: 0
+                    });
                 });
 
-                $("[data-discoverable]").click(function() {
+                $(document).on("click", "[data-discoverable]", function () {
                     if ($(this).attr("data-discoverable") == "open") {
                         $(this).attr("data-discoverable", true);
                         $("#" + $(this).attr("data-list")).slideUp(300);
@@ -252,7 +244,7 @@ jQuery.fn.extend({
                     }
                 });
 
-                $.each($("[data-discoverable='open']"), function(i, v) {
+                $.each($("[data-discoverable='open']"), function (i, v) {
                     var attr = $(v).attr("data-list");
                     $("#" + attr).show();
                 });
@@ -261,180 +253,191 @@ jQuery.fn.extend({
                 //     $(this).parent("[data-dropdown]").slideUp();
                 // })
             }
-        }
+        };
 
         if (typeof param1 == 'undefined') {
             main[func]();
         } else {
             main[func](param1);
         }
-    },
+    }
 });
 
+var Input = function Input() {
+    var that = this;
 
-var Input = function() {
-        var that = this;
-
-        this.keyboard = function(event) {
-            var key = event.which;
-            if ((key >= 48 && key <= 222) || key == 8 || key == 36) {
-                return true;
-            } else {
-                return false;
-            }
+    function keyboard(event) {
+        var key = event.which;
+        if (key >= 48 && key <= 222 || key == 8 || key == 36) {
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        this.Validate = function() {
-            this.alphanum = function(val) {
-                return /^[a-zA-Z0-9\x20]+$/.test(val);
-            }
+    function Validate() {
+        this.alphanum = function (val) {
+            return (/^[a-zA-Z0-9\x20]+$/.test(val)
+            );
+        };
 
-            this.alpha = function(val) {
-                return /^[a-zA-Z\x20]+$/.test(val);
-            }
+        this.alpha = function (val) {
+            return (/^[a-zA-Z\x20]+$/.test(val)
+            );
+        };
 
-            this.number = function(val) {
-                return /^[0-9]+$/.test(val);
-            }
-        }
+        this.number = function (val) {
+            return (/^[0-9]+$/.test(val)
+            );
+        };
+    }
 
-        this.Write = function() {
-            this.error = function(a) {
-                a.css("borderColor", "red");
-                return a;
-            }
+    function Write() {
+        this.error = function (a) {
+            a.css("borderColor", "red");
+            return a;
+        };
 
-            this.success = function(a) {
-                a.css("borderColor", "#00FF00");
-                return a;
-            }
+        this.success = function (a) {
+            a.css("borderColor", "#00FF00");
+            return a;
+        };
 
-            this.normal = function(a) {
-                a.css("borderColor", "#AAAAFF");
-                return a;
-            }
-        }
-    },
-    Anderlyne = function() {
-        var that = this;
+        this.normal = function (a) {
+            a.css("borderColor", "#AAAAFF");
+            return a;
+        };
+    }
 
-        that.notifWindow = function(str, title) {
-            if (typeof title == 'undefined') {
-                title = null;
-            }
-
-
-            if (title != null) {
-                $("#rev-notif").find(".title").html("<h2>" + str + "</h2>");
-            }
-
-            if (str != null) {
-                $("#rev-notif").find(".body").html("<p>" + str + "</p>");
-                $("#rev-notif").anderlyne('modal', "open");
-            }
-
-            return that;
-        }
-
-        that.notifBar = function(str, title, style) {
-            if (typeof title == "undefined") {
-                title = null;
-                style = "default";
-            }
-
-            if (typeof style == "undefined") {
-                style = "default";
-            }
-
-            var a = "#anderlyne-notif-bar";
-            if (title == null) {
-                title = "Notification";
-            }
-            $(a).removeClass();
-            $(a).addClass(style);
-            $(a + " h3").html(title);
-            $(a + " p").html(str);
-            $(a).css("left", "0");
-
-            countDownTimer({
-                count: 0,
-                limit: 5,
-                time: 1000,
-                success: function() {
-                    $(a).css("left", "-1000px");
-                }
-            });
-
-            return that;
-        }
-
-        that.notifFly = function(str, style) {
-            if (typeof style == "undefined") {
-                style = "default";
-            }
-
-            var a = "#anderlyne-notif-fly";
-            $(a).removeClass();
-            $(a).addClass(style);
-            $(a + " p").html(str);
-            $(a).css("top", "30px");
-            countDownTimer({
-                count: 0,
-                limit: 5,
-                time: 1000,
-                success: function() {
-                    $("#anderlyne-notif-fly").css("top", "-100px");
-                }
-            });
-
-            return that;
-        }
-
-        that.contextMenu = function(config) {
-            $("body").append('<table id="' + config.id + '" data-context-menu>\
-                    <tbody>\
-                        \
-                    </tbody>\
-                </table>');
-
-            var t;
-
-            if (typeof config.init != 'undefined') {
-                config.init();
-            }
-
-            var x = config.items;
-            $.each(x, function(i, v) {
-                var icon = '';
-                if (typeof v['icon'] != "undefined")
-                    icon = '<td class="ico">' + v['icon'] + '</td>';
-
-                t = $("#" + config.id + " tbody").append('<tr>' +
-                    icon +
-                    '<td>' + v['name'] + '</td>\
-                        </tr>');
-
-                var curItem = $(t)[0],
-                    child = $(curItem).children()[i];
-
-                if (typeof v['action'] != 'undefined') {
-                    $(child).click(function() {
-                        var y = v['action'];
-                        y();
-                    })
-                }
-            });
-
-            return that;
-        }
+    return {
+        keyboard: keyboard,
+        Validate: Validate,
+        Write: Write
     };
-var anderlyne = new Anderlyne();
+},
+    anderlyne = function () {
+    var that = this;
+
+    function notifWindow(str, title) {
+        if (typeof title == 'undefined') {
+            title = null;
+        }
+
+        if (title != null) {
+            $("#rev-notif").find(".title").html("<h2>" + str + "</h2>");
+        }
+
+        if (str != null) {
+            $("#rev-notif").find(".body").html("<p>" + str + "</p>");
+            $("#rev-notif").anderlyne('modal', "open");
+        }
+
+        return that;
+    }
+
+    function notifBar(str, title, style) {
+        if (typeof title == "undefined") {
+            title = null;
+            style = "default";
+        }
+
+        if (typeof style == "undefined") {
+            style = "default";
+        }
+
+        var a = "#anderlyne-notif-bar";
+        if (title == null) {
+            title = "Notification";
+        }
+        $(a).removeClass();
+        $(a).addClass(style);
+        $(a + " h3").html(title);
+        $(a + " p").html(str);
+        $(a).css("left", "0");
+
+        countDownTimer({
+            count: 0,
+            limit: 5,
+            time: 1000,
+            success: function success() {
+                $(a).css("left", "-1000px");
+            }
+        });
+
+        return that;
+    }
+
+    function notifFly(str, style) {
+        if (typeof style == "undefined") {
+            style = "default";
+        }
+
+        var a = "#anderlyne-notif-fly";
+        $(a).removeClass();
+        $(a).addClass(style);
+        $(a + " p").html(str);
+        $(a).css("top", "30px");
+        countDownTimer({
+            count: 0,
+            limit: 5,
+            time: 1000,
+            success: function success() {
+                $("#anderlyne-notif-fly").css("top", "-100px");
+            }
+        });
+
+        return that;
+    }
+
+    function contextMenu(config) {
+        $("body").append('<table id="' + config.id + '" data-context-menu>\
+                        <tbody>\
+                            \
+                        </tbody>\
+                    </table>');
+
+        var t;
+
+        if (typeof config.init != 'undefined') {
+            config.init();
+        }
+
+        var x = config.items;
+        $.each(x, function (i, v) {
+            var icon = '';
+            if (typeof v['icon'] != "undefined") icon = '<td class="ico">' + v['icon'] + '</td>';
+
+            t = $("#" + config.id + " tbody").append('<tr>' + icon + '<td>' + v['name'] + '</td>\
+                            </tr>');
+
+            var curItem = $(t)[0],
+                child = $(curItem).children()[i];
+
+            if (typeof v['action'] != 'undefined') {
+                $(child).click(function () {
+                    var y = v['action'];
+                    y();
+                });
+            }
+        });
+
+        return that;
+    }
+
+    return {
+        notifWindow: notifWindow,
+        notifBar: notifBar,
+        notifFly: notifFly,
+        contextMenu: contextMenu
+    };
+}();
+
 var input = new Input();
 
 function countDownTimer(setup) {
     setup.count++;
     if (setup.count < setup.limit) {
-        setTimeout(function() {
+        setTimeout(function () {
             countDownTimer(setup);
         }, setup.time);
     } else if (setup.count >= setup.limit) {
